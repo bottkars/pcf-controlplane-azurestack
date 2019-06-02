81965b9ec8975605d913296354e25da4f0fc838c
PCF Developer <pcfuser@labbuildr.com>
2019-06-02 08:56:32 +0200

    opsmanpublicip

---
+++ prepare_utils.ps1
@@ -0,0 +1,114 @@
+### prepare utils
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   receives OpenSSL, se get-help rEceive-LabOpenSSL -online for details
+.LINK
+   https://github.com/bottkars/labtools/wiki/Receive-LABOpenSSL
+.EXAMPLE
+#>
+#requires -version 3
+function Receive-LABOpenSSL
+{
+[CmdletBinding(DefaultParametersetName = "1",
+    SupportsShouldProcess=$true,
+    ConfirmImpact="Medium")]
+	[OutputType([psobject])]
+param(
+    [Parameter(ParameterSetName = "1", Mandatory = $false)]
+    $Destination="./",
+    [Parameter(ParameterSetName = "1", Mandatory = $false)]
+    [ValidateSet(
+	'1_0_1','1_0_2','1_1_0'
+    )]
+    [string]$OpenSSL_Ver="1_0_2"
+)
+
+if (Test-Path -Path "$Destination")
+    {
+    Write-Host -ForegroundColor Gray " ==>$Destination found"
+    }
+else
+    {
+    Write-Host -ForegroundColor Gray " ==>Creating Sourcedir for OpenSSL"
+    New-Item -ItemType Directory -Path $Destination -Force | Out-Null
+    }
+Write-Host -ForegroundColor Gray " ==>Checking for latest OpenSSL light"
+
+$OpenSSL_URL = "https://slproweb.com/products/Win32OpenSSL.html"
+try
+	{
+	$Req = Invoke-WebRequest  -UseBasicParsing -Uri $OpenSSL_URL
+	}
+catch 
+	{
+	Write-Host "Error getting OpenSSL"
+	$_
+	break
+	}
+try 
+	{
+	Write-Host -ForegroundColor Gray " ==>Trying to Parse OpenSSL Site $OpenSSL_URL"
+	$Parse = $Req.Links | where-object {$_ -Match "Win64OpenSSL_Light-$OpenSSL_Ver"}
+	#https://slproweb.com/download/Win32OpenSSL_Light-1_0_1t.exe
+	} 
+catch
+	{
+	Write-Host -ForegroundColor Yellow "Error Parsing"
+	$_
+	break
+	}
+
+$File = ($Parse | Select-Object -First 1).outerHTML
+$link = $File.Split('"') | where-object {$_ -Match "/download"}
+$URL = "https://slproweb.com" + $($link)
+Write-Verbose " ==>got $URL"
+    $FileName = Split-Path -Leaf -Path $Url
+    if (!(test-path  "$Destination\$FileName"))
+        {
+        Write-Host -ForegroundColor Gray " ==>$FileName not found, trying to download $Filename"
+        Start-BitsTransfer  $URL -destination "$Destination\$FileName"
+        }
+    else
+        {
+        Write-Host -ForegroundColor Gray  " ==>found $Filename in $Destination"
+        }
+$Version = $FileName -replace "Win64OpenSSL_Light-"
+$Version = $Version -replace ".exe"
+$object = New-Object psobject
+$object | Add-Member -MemberType NoteProperty -Name Filename -Value $FileName
+$object | Add-Member -MemberType NoteProperty -Name Version -Value $Version
+Write-Output $object 
+}
+
+$global:vmxtoolkit_type="win_x86_64"
+Write-Host "installing module pivposh"
+Install-Module pivposh -scope CurrentUser -Force
+Write-Host "installing om-cli"
+Install-Script Download-Om -Force -Scope CurrentUser -MinimumVersion 1.1
+$OM = Download-Om -DownloadDir "$($HOME)/om" -OmRelease 1.1.0
+Write-Host "Installing cf-cli Installer"
+Install-Script install-cf-cli -Scope CurrentUser
+Install-cf-cli.ps1 -CLIRelease '6.43.0' -DownloadDir "$HOME\Downloads" 
+Write-Host "Installing cf-uaac Installer"
+Install-Script install-cf-uaac -Scope CurrentUser
+Install-cf-uaac.ps1 
+
+Write-Host "Installing BoSH Installer"
+Install-Script install-bosh -Scope CurrentUser
+Install-bosh.ps1 -BoshRelease 'v5.5.1' -DownloadDir "$HOME/bosh" 
+Write-Host "Installing CredHUB Installer"
+Install-Script install-credhub-cli -Scope CurrentUser
+Install-credhub-cli.ps1  -CredhubRelease '2.4.0' -DownloadDir "$HOME/credhub"
+
+
+Write-Host "Installing OpenSSL"  
+$OpenSSL=Receive-LABOpenSSL -Destination "$($HOME)/Downloads" -OpenSSL_Ver 1_1_0 
+$OpenSSLArgs = '/silent'
+$Setuppath = "$($HOME)/Downloads/$($OpenSSL.Filename)"
+unblock-file $Setuppath
+Start-Process -FilePath $Setuppath -ArgumentList $OpenSSLArgs -PassThru -Wait
+
+
---
+++ scripts/get-lateststemcells.ps1
@@ -0,0 +1,82 @@
+#requires -module pivposh
+param(
+    [Parameter(Mandatory = $false)]	
+    [Validatescript( {Test-Path -Path $_ })]
+    $DIRECTOR_CONF_FILE="$HOME/director_control.json",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('170', '250','97','3586','3541')]
+    [string[]]$Families= '170',
+    [Parameter(Mandatory = $false)]
+    [switch]$apply
+)
+Push-Location $PSScriptRoot
+$director_conf = Get-Content $DIRECTOR_CONF_FILE | ConvertFrom-Json
+$OM_Target = $director_conf.OM_TARGET
+$downloaddir = $director_conf.downloaddir
+# getting the env
+$env_vars = Get-Content $HOME/env.json | ConvertFrom-Json
+$env:Path = "$($env:Path);$HOME/OM"
+$PIVNET_UAA_TOKEN = $env_vars.PIVNET_UAA_TOKEN
+
+$access_token = Get-PIVaccesstoken -refresh_token $PIVNET_UAA_TOKEN
+
+
+$branchs = @()
+
+foreach ($Family in $Families) 
+{
+    switch ($Family){
+        '3586' {
+                $branchs += Get-PIVRelease -id 82 | where-object version -Match 3586. | Select-Object -First 1
+        }
+        '3541' {
+                $branchs += Get-PIVRelease -id 82 | where-object version -Match 3541. | Select-Object -First 1
+        }
+        default {
+                $branchs += Get-PIVRelease -id 233 | where-object version -Match "$Family." | Select-Object -First 1    
+            }
+    }
+}
+
+if ($apply.IsPresent)
+    {
+        $floating = "true"
+    }
+else {
+    $floating = "false"
+}    
+foreach ($branch in $branchs) {
+Write-Host  "Accepting EULA for Slug $($branch.slugid) Release $($branch.id)"
+$eula =    $branch | Confirm-PIVEula -access_token $access_token
+$output_directory = New-Item -ItemType Directory -Path "$downloaddir/stemcells/$($branch.version)" -Force 
+$aws_object_key = ($branch | Get-PIVFileReleaseId | where-object aws_object_key -match "hyperv").aws_object_key
+$stemcell_real_filename = Split-Path -Leaf $aws_object_key
+
+Write-Host "Stemcell filename $stemcell_real_filename"
+ om --env $HOME/om_$($director_conf.RG).env `
+--request-timeout 7200 `
+download-product `
+--pivnet-api-token $PIVNET_UAA_TOKEN `
+--pivnet-file-glob $stemcell_real_filename `
+--pivnet-product-slug $branch.slugid `
+--product-version $branch.version `
+--output-directory $output_directory.FullName
+
+$download_file = get-content "$($output_directory.FullName)/download-file.json" | ConvertFrom-Json
+$STEMCELL_FILENAME = $download_file.product_path
+# Copy-Item  -Path "$STEMCELL_FILENAME" -Destination "$($output_directory.FullName)/$stemcell_real_filename"
+ om --env $HOME/om_$($director_conf.RG).env `
+    upload-stemcell `
+    --floating=$floating `
+    --stemcell $STEMCELL_FILENAME
+}
+
+#    --stemcell "$($output_directory.FullName)/$stemcell_real_filename"
+if ($apply.IsPresent)
+    {
+     om --env $HOME/om_$($director_conf.RG).env `
+    apply-changes `
+    --skip-unchanged-products
+    }
+
+Pop-Location 
